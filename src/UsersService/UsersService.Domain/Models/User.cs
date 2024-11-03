2edfd693a9db6c00904cb57d94e2d10e7f564cff
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-11-03 21:53:12 +0300

    Add user model with domain exceptions

---
+++ src/UsersService/UsersService.Domain/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UsersService.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Type { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
