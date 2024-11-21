7b1f0c8967277164640ac3dec6684f009d29b408
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-11-21 16:01:53 +0300

    Add specifications for vacancies details filtering

---
+++ src/VacanciesService/VacanciesService.Infrastructure/NoSQL/Specifications/VacancyLanguagesSpecification.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using System.Text.RegularExpressions;
+using VacanciesService.Domain.Entities.NoSQL;
+using VacanciesService.Domain.Filters.VacancyDetails;
+
+namespace VacanciesService.Infrastructure.NoSQL.Specifications
+{
+    public class VacancyLanguagesSpecification : Specification<VacancyDetailsEntity>
+    {
+        public VacancyLanguagesSpecification(IEnumerable<LanguageFilter> languageFilters)
+            : base(GetFilter(languageFilters))
+        {
+        }
+
+        private static FilterDefinition<VacancyDetailsEntity> GetFilter(IEnumerable<LanguageFilter> languageFilters)
+        {
+            var filters = new List<FilterDefinition<VacancyDetailsEntity>>();
+
+            foreach (var language in languageFilters)
+            {
+                var languageFilter = Builders<LanguageEntity>.Filter.And(
+                    Builders<LanguageEntity>.Filter.Regex(l => l.Name, new Regex(language.Name, RegexOptions.IgnoreCase)),
+                    Builders<LanguageEntity>.Filter.Regex(l => l.Level, new Regex(language.Level, RegexOptions.IgnoreCase)));
+
+                filters.Add(Builders<VacancyDetailsEntity>.Filter.ElemMatch(vc => vc.Languages, languageFilter));
+            }
+
+            var filter =
+                Builders<VacancyDetailsEntity>.Filter.Exists(vc => vc.Languages) &
+                Builders<VacancyDetailsEntity>.Filter.Or(filters);
+
+            return filter;
+        }
+    }
+}
---
+++ src/VacanciesService/VacanciesService.Infrastructure/NoSQL/Specifications/VacancySalarySpecification.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using VacanciesService.Domain.Entities.NoSQL;
+using VacanciesService.Domain.Filters.VacancyDetails;
+
+namespace VacanciesService.Infrastructure.NoSQL.Specifications
+{
+    public class VacancySalarySpecification : Specification<VacancyDetailsEntity>
+    {
+        public VacancySalarySpecification(SalaryFilter salaryFilter)
+            : base(GetFilter(salaryFilter))
+        {
+        }
+
+        private static FilterDefinition<VacancyDetailsEntity> GetFilter(SalaryFilter salaryFilter)
+        {
+            var filter = Builders<VacancyDetailsEntity>.Filter.Exists(vd => vd.Salary);
+
+            if (salaryFilter.Min is not null && salaryFilter.Max is not null)
+            {
+                filter &= Builders<VacancyDetailsEntity>.Filter.And(
+                    Builders<VacancyDetailsEntity>.Filter.Lte(vd => vd.Salary.Min, salaryFilter.Max),
+                    Builders<VacancyDetailsEntity>.Filter.Gte(vd => vd.Salary.Max, salaryFilter.Min));
+            }
+            else if (salaryFilter.Min is not null)
+            {
+                filter &= Builders<VacancyDetailsEntity>.Filter.Gte(vd => vd.Salary.Max, salaryFilter.Min);
+            }
+            else if (salaryFilter.Max is not null)
+            {
+                filter &= Builders<VacancyDetailsEntity>.Filter.Lte(vd => vd.Salary.Min, salaryFilter.Max);
+            }
+
+            return filter;
+        }
+    }
+}
