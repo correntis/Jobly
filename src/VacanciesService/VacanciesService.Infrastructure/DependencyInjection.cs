36b4886b7fb212e7c7beb5ba93a1c86c766827fb
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-11-21 16:00:27 +0300

    Add contexts, repositories and configure docker containers

--- src/VacanciesService/VacanciesService.Infrastructure/DependencyInjection.cs
+++ src/VacanciesService/VacanciesService.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,43 @@
-﻿namespace VacanciesService.Infrastructure
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VacanciesService.Domain.Abstractions.Contexts;
+using VacanciesService.Domain.Abstractions.Repositories;
+using VacanciesService.Domain.Abstractions.Services;
+using VacanciesService.Domain.Configuration;
+using VacanciesService.Infrastructure.API.Services;
+using VacanciesService.Infrastructure.NoSQL;
+using VacanciesService.Infrastructure.NoSQL.Repositories;
+using VacanciesService.Infrastructure.SQL;
+
+namespace VacanciesService.Infrastructure
 {
     public static class DependencyInjection
     {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MongoDbOptions>(configuration.GetSection(nameof(MongoDbOptions)));
+            services.Configure<CurrencyApiOptions>(configuration.GetSection(nameof(CurrencyApiOptions)));
+
+            services.AddDbContext<VacanciesReadContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("ReadPostgreDatabase"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddDbContext<VacanciesWriteContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("WritePostgreDatabase"));
+            });
+
+            services.AddSingleton<IMongoDbContext, MongoDbContext>();
+
+            services.AddScoped<IVacanciesReadContext, VacanciesReadContext>();
+            services.AddScoped<IVacanciesWriteContext, VacanciesWriteContext>();
+            services.AddScoped<IVacanciesDetailsRepository, VacanciesDetailsRepository>();
+
+            services.AddScoped<ICurrencyApiService, CurrencyApiServiceDevelopmentMock>();
+            //services.AddScoped<ICurrencyApiService, CurrencyApiService>();
+        }
     }
 }
---
+++ src/VacanciesService/VacanciesService.Infrastructure/SQL/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VacanciesService.Domain.Constants;
+using VacanciesService.Domain.Entities.SQL;
+
+namespace VacanciesService.Infrastructure.SQL.Configuration
+{
+    public class ApplicationConfiguration : IEntityTypeConfiguration<ApplicationEntity>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationEntity> builder)
+        {
+            builder.ToTable("Applications");
+
+            builder.HasKey(a => a.Id);
+
+            builder.HasIndex(a => a.UserId);
+
+            builder.Property(a => a.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(a => a.Status)
+                .HasMaxLength(BusinessRules.Application.StatusMaxLength)
+                .IsRequired();
+        }
+    }
+}
