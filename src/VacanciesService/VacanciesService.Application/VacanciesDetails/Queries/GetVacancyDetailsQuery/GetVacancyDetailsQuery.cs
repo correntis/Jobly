3d1c11d6ec7d347e264aaf0747899244c3faf9d8
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-11-21 16:36:45 +0300

    Add GET endpoint for vacancy details

---
+++ src/VacanciesService/VacanciesService.Application/VacanciesDetails/Queries/GetVacancyDetailsQuery/GetVacancyDetailsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using VacanciesService.Domain.Models;
+
+namespace VacanciesService.Application.VacanciesDetails.Queries.GetVacancyDetailsQuery
+{
+    public sealed record GetVacancyDetailsQuery(int VacancyId) : IRequest<VacancyDetails>;
+}
