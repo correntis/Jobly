d8dee9e1551de9c39d95de726fee9c8337899458
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-11-27 17:07:01 +0300

    Remove old docker composes and add business rules for token

--- src/VacanciesService/VacanciesService.Domain/Constants/BusinessRules.cs
+++ src/VacanciesService/VacanciesService.Domain/Constants/BusinessRules.cs
@@ -19,5 +19,22 @@
             public const string DefaultCurrency = "USD";
             public const string DefaultCurrencyType = "fiat";
         }
+
+        public static class Token
+        {
+            public const double AccessTokenExpiresDays = 1;
+            public const double RefreshTokenExpiresDays = 7;
+
+            public const string AccessTokenName = "AccessToken";
+            public const string RefreshTokenName = "RefreshToken";
+        }
+
+        public static class Roles
+        {
+            public const string Company = "Company";
+            public const string User = "User";
+
+            public static readonly IEnumerable<string> All = [Company, User];
+        }
     }
 }
