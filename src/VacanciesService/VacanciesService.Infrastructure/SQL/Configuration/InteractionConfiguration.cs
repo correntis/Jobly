1bc0d7b7bab22b731fa4bbf1ac76996d5b2d5581
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-12-07 09:43:19 +0300

    Add vacancy interaction entity

---
+++ src/VacanciesService/VacanciesService.Infrastructure/SQL/Configuration/InteractionConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VacanciesService.Domain.Entities.SQL;
+
+namespace VacanciesService.Infrastructure.SQL.Configuration
+{
+    public class InteractionConfiguration : IEntityTypeConfiguration<VacancyInteractionEntity>
+    {
+        public void Configure(EntityTypeBuilder<VacancyInteractionEntity> builder)
+        {
+            builder.ToTable("Interactions");
+
+            builder.HasKey(i => i.Id);
+
+            builder.HasIndex(i => i.UserId);
+            builder.HasIndex(i => i.VacancyId);
+
+            builder.Property(i => i.Id)
+                .HasColumnType("uuid")
+                .ValueGeneratedOnAdd()
+                .HasDefaultValueSql("gen_random_uuid()");
+
+            builder.Property(i => i.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasConversion(
+                    i => DateTime.SpecifyKind(i, DateTimeKind.Unspecified),
+                    i => DateTime.SpecifyKind(i, DateTimeKind.Local))
+                .IsRequired();
+        }
+    }
+}
