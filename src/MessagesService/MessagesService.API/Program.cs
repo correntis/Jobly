9264062033cfc8f996f3cd0bd93e1a31c833fd79
Maksim Rusetcki <maksim.rusetckiy05@mail.ru>
2024-12-17 21:30:57 +0300

    Configure serilog and stylecop

--- src/MessagesService/MessagesService.API/Program.cs
+++ src/MessagesService/MessagesService.API/Program.cs
@@ -1,9 +1,21 @@
+using Elastic.Serilog.Sinks;
 using MessagesService.Application;
 using MessagesService.DataAccess;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .Enrich.WithEnvironmentName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch([new Uri(context.Configuration["Elastic:Uri"])])
+        .ReadFrom.Configuration(context.Configuration);
+});
 
 services.AddApplication();
 services.AddDataAccess(configuration);
@@ -14,11 +26,8 @@ services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
